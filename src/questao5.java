dce022a975ef4f53ce899fd1d3663947b70a1870
JulioHerculano <julyocesarplay@gmail.com>
2023-04-18 23:08:28 -0300

    finalmente consegui

--- src/questao5.java
+++ src/questao5.java
@@ -1,19 +1,31 @@
+/*5. Faça um programa que determine o fatorial de um número. Para 
+este problema, tem-se como entrada o valor do número do qual 
+se deseja calcular o fatorial. O fatorial de 0 é igual a 1. O 
+fatorial de um número N (N!) é definido conforme a seguir 
+(utilize for):
+i. N! = 1 * 2 * 3 * 4 * ... * (N-1) * N
+ */
 import java.util.Scanner;
 
-public class questao5 {
+ public class questao5 {
     public static void main(String[] args) {
-        int fatorial = 0;
-        int n;
-        int i;
+        int numeroInserido;
+        int Fatorial = 1;
+        int i; //contador
 
+        System.out.println("Insira um numero que deseja saber o fatorial: ");
         Scanner teclado = new Scanner(System.in);
-        n = teclado.nextInt();
-        for (i = 1; i <= n; i = i + 1) {
-            fatorial = fatorial * i; // 100
-        }
+        numeroInserido = teclado.nextInt();
 
-        System.out.println(fatorial); // 1
-        }
+        for (i = 1; i <= numeroInserido; i = i + 1) {
+           
+            Fatorial = Fatorial* i;
+            }
+
+        System.out.printf("o fatorial de %d é igual a %d.", numeroInserido,Fatorial);  
+
+        } 
+        
 
     }
 
